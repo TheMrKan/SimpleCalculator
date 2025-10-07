b557c426e65e76664c7ee21b964026244ad28fe6
TheMrKan <mrkan2017@gmail.com>
2025-10-07 14:10:09 +0300

    docstring и тесты для операторов

--- src/operators.py
+++ src/operators.py
@@ -4,6 +4,10 @@ from functools import wraps
 
 
 class OperationError(Exception):
+    """
+    Ошибка при выполнении оператора
+    """
+
     left: float
     right: float
     operator: str
@@ -16,15 +20,30 @@ class OperationError(Exception):
         self.comment = comment
 
     def __str__(self) -> str:
+        """
+        Считается в функции, т. к. comment может присваиваться после инициализации
+        """
         return f"Недопустимая операция: {self.left} {self.operator} {self.right}{f" - {self.comment}" if self.comment else ""}"
 
 
 class BinaryOperator:
+    """
+    Предполагается, что экземпляр оператора создается один раз и дальше используется как Flyweight через from_symbol.
+    Экземпляр оператора является callable, т. е. используется как op_instance(left, right).
+    """
     __func: Callable[[float, float], float]
     __str_repr: str
+    """
+    Строковое представление оператора для отладки и вывода ошибок
+    """
 
-    @classmethod
-    def from_symbol(cls, sym: str):
+    @staticmethod
+    def from_symbol(sym: str) -> 'BinaryOperator':
+        """
+        Для получения экземпляра оператора по символу
+        :raises KeyError: Оператор не определен
+        :return: Flyweight экземпляр оператора
+        """
         return _OP_MAP[sym]
 
     def __init__(self, str_repr: str, func: Callable[[float, float], float]):
@@ -32,6 +51,12 @@ class BinaryOperator:
         self.__str_repr = str_repr
 
     def __call__(self, left: float, right: float) -> float:
+        """
+        Применяет оператор, аналог 'left ? right', где ? - оператор (например +).
+        Все вычисления проводятся над float.
+        :raises OperationError: Если произошла заранее обработанная ошибка (например, переполнение)
+        :return: Результат выполнения с точностью 2 знака после запятой
+        """
         try:
             return round(float(self.__func(left, right)), 2)
         except OperationError as e:
@@ -48,6 +73,10 @@ class BinaryOperator:
 
 
 def assert_right_is_non_zero(func: Callable[[float, float], float]) -> Callable[[float, float], float]:
+    """
+    Декоратор, проверяющий, что правый операнд не ноль.
+    :raises OperationError: Правой операнд 0
+    """
     @wraps(func)
     def wrapper(left: float, right: float) -> float:
         if right == 0:
@@ -57,11 +86,15 @@ def assert_right_is_non_zero(func: Callable[[float, float], float]) -> Callable[
     return wrapper
 
 
-def assert_right_is_integer(func: Callable[[float, float], float]) -> Callable[[float, float], float]:
+def assert_integers(func: Callable[[float, float], float]) -> Callable[[float, float], float]:
+    """
+    Декоратор, проверяющий, что оба операнда - целые числа.
+    :raises OperationError: Хотя бы один из операндов не является целым числом
+    """
     @wraps(func)
     def wrapper(left: float, right: float) -> float:
-        if not right.is_integer():
-            raise OperationError(left, "", right, "операция допустима только над целым числом")
+        if not right.is_integer() or not left.is_integer():
+            raise OperationError(left, "", right, "операция допустима только над целыми числами")
         return func(left, right)
     return wrapper
 
@@ -71,7 +104,10 @@ _OP_MAP = {
     "-": BinaryOperator("-", ops.sub),
     "*": BinaryOperator("*", ops.mul),
     "/": BinaryOperator("/", assert_right_is_non_zero(ops.truediv)),
-    "#": BinaryOperator("//", assert_right_is_non_zero(assert_right_is_integer(ops.floordiv))),
-    "%": BinaryOperator("%", assert_right_is_integer(ops.mod)),
+    "#": BinaryOperator("//", assert_right_is_non_zero(assert_integers(ops.floordiv))),
+    "%": BinaryOperator("%", assert_right_is_non_zero(assert_integers(ops.mod))),
     "^": BinaryOperator("**", ops.pow),
 }
+"""
+Flyweight объекты операторов
+"""
---
+++ tests/test_operators.py
@@ -0,0 +1,71 @@
+from unittest import TestCase
+
+from src.operators import BinaryOperator as Op, OperationError
+
+
+class TestSimpleOperators(TestCase):
+
+    def test_sum(self):
+        self.assertEqual(11.5, Op.from_symbol("+")(13.6, -2.1))
+
+    def test_sub(self):
+        self.assertEqual(11.5, Op.from_symbol("-")(13.6, 2.1))
+
+    def test_mul(self):
+        self.assertEqual(9, Op.from_symbol("*")(6, 1.5))
+
+    def test_pow(self):
+        self.assertEqual(256, Op.from_symbol("^")(2, 8))
+
+
+class TestTrueDiv(TestCase):
+
+    def test_simple(self):
+        self.assertEqual(1.5, Op.from_symbol("/")(6, 4))
+
+    def test_zero_division(self):
+        with self.assertRaises(OperationError):
+            Op.from_symbol("/")(6, 0)
+
+
+class TestFloorDiv(TestCase):
+
+    def test_simple(self):
+        self.assertEqual(1, Op.from_symbol("#")(6, 4))
+
+    def test_zero_division(self):
+        with self.assertRaises(OperationError):
+            Op.from_symbol("#")(6, 0)
+
+    def test_left_float(self):
+        with self.assertRaises(OperationError):
+            Op.from_symbol("#")(6.5, 4)
+
+    def test_right_float(self):
+        with self.assertRaises(OperationError):
+            Op.from_symbol("#")(4, 6.5)
+
+
+class TestModDiv(TestCase):
+
+    def test_simple(self):
+        self.assertEqual(1, Op.from_symbol("%")(6, 5))
+
+    def test_zero_division(self):
+        with self.assertRaises(OperationError):
+            Op.from_symbol("%")(6, 0)
+
+    def test_left_float(self):
+        with self.assertRaises(OperationError):
+            Op.from_symbol("%")(6.5, 4)
+
+    def test_right_float(self):
+        with self.assertRaises(OperationError):
+            Op.from_symbol("%")(4, 6.5)
+
+
+class TestExtra(TestCase):
+
+    def test_overflow(self):
+        with self.assertRaises(OperationError):
+            Op.from_symbol("^")(6, 1000)
